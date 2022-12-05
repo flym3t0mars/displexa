74317fd9774e7fe25aaa5ba79dda98571d06d32b
Carlos Gudino <cgudino95@me.com>
2022-12-04 17:29:29 -0800

    cleaned up response and logging

--- main.py
+++ main.py
@@ -5,28 +5,32 @@ import discord
 from dotenv import load_dotenv
 from time import sleep
 from os import system as s
-s('clear')
-print('...loading...')
+import imdb
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-#client = discord.Client()
-
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-print('here')
-
 down='☑️'
 complete='✅'
+
 #############
 # KEY WORDS #
 #############
+
 a='add '
 null='[]'
+
+@client.event
+async def isearch(a):
+    iq = imdb.IMDb()
+    search = iq.search_movie(a)
+    for movies in search:
+        print(movies)
 
 @client.event
 async def on_ready():
@@ -46,8 +50,7 @@ async def on_ready():
     print(f'Guild Members:\n - {members}')
     sleep(1)
     s('clear')
-        
-    
+
 @client.event
 async def on_member_join(member):
     await member.create_dm()
@@ -63,27 +66,15 @@ async def on_message(message):
 
     if a in message.content.lower():
         split_=f'{message.content.split(a,len(message.content))[1:len(message.content)]}'
-        
-        # log this also who sent it
-        if message.content!=null:
-            print(message.content)
         mess=(''.join(split_))
-        
-        if mess==null:
-            response="someone made a mistake.\nif you're on mobile\n **don't forget to request with (add add {movie})**"
-        else:
-            response=f"downloading: \n{down}{mess}"
-        
+        response=f"downloading: \n{down} {mess}"
         await message.channel.send(response)
-        
+
         logging=print(mess)
-        if logging==null:
-            print('someone made a mistake.')
-        else:
+        if logging!=null:
             logging
-     
-    with open('movieRequests.log', 'a') as f:
-        f.write(str(message.author)+': '+mess+'\n')
-        
-        
+
+        with open('movieRequests.log', 'a') as f:
+            f.write(str(message.author)+': '+mess+'\n')
+
 client.run(TOKEN)
