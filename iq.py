bf20e2acca637fff8534ac30b74a028f5edff295
Carlos Gudino <cgudino95@me.com>
2022-12-04 23:17:51 -0800

    fixing search to have movie ID

--- iq.py
+++ iq.py
@@ -9,10 +9,17 @@ def isearch(a):
 	for i in range(len(search)):
 		media.append(search[i])
 		global s
-		s+=(f'{[i+1]} {media[i]} \n')		
+		s+=(f'{[i+1]} {media[i]} \n')
+	print(s)
+	print('pick a number between 1-20')
+	t=int(input('Number: '))
+	print(f'you picked: {media[t-1]}')
+	# how to get tt3778644
+	print(f'tt{media[t-1].movieID}')
+		
 def main():
 	isearch(input('movie: '))
-	print(s)
+	# print(s)
 
 if __name__=="__main__":
 	main()
