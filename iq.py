9f472307d01164136c3d24378099443e4676f65d
Carlos Gudino <cgudino95@me.com>
2022-12-04 21:35:37 -0800

    search in one message

--- iq.py
+++ iq.py
@@ -3,29 +3,15 @@ import imdb
 media=[]
 s= ""
 
-#for i in range(len(n)):
-#...     s+=(f'{i+1} {n[i]}\n')
-
-
 def isearch(a):
 	iq = imdb.IMDb()
 	search = iq.search_movie(a)
 	for i in range(len(search)):
-		#print(movies)
 		media.append(search[i])
 		global s
 		s+=(f'{[i+1]} {media[i]} \n')		
 def main():
 	isearch(input('movie: '))
-	#for i in range(len(media)):
-	#	print(f'{i+1} {media[i]}')
-	#print('------')
-	#print(f'tt{media[0].movieID}')  # how to get TT
-	#print(media[1])
-	#print('#####')
-
-
-	# print('\n'.join([str(elem) for elem in media]) + '[1]')
 	print(s)
 
 if __name__=="__main__":
